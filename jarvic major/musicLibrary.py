b4107c97a49dd00deab2745c4fdc2499f80ae69e
PratibhaSuryawanshi <pratibhasuryawanshi411@gmail.com>
2025-09-10 05:56:07 +0530

    Add files via upload

---
+++ jarvic major/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "ritu" : "https://www.youtube.com/watch?v=NiPf1D-nxN8",
+    "music" : "https://www.youtube.com/watch?v=4201gKyNeII",
+    "skyfall" : "https://www.youtube.com/watch?v=sZrTJesvJeo&list=RDsZrTJesvJeo&start_radio=1",
+    "song" : "https://www.youtube.com/watch?v=B-J_PuEhyOM&list=RDB-J_PuEhyOM&start_radio=1",
+    "drop" : "https://www.youtube.com/watch?v=0KtJMUxpxCA&list=RD0KtJMUxpxCA&start_radio=1"
+}
